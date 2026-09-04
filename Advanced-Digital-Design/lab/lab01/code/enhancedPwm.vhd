7d2807ac570f99d2f5c5416eef003ce47bbfc0db
Christopher Coulston <coulston@mines.edu>
2026-09-03 21:15:23 -0600

    update pwm lab with some starter code

---
+++ Advanced-Digital-Design/lab/lab01/code/enhancedPwm.vhd
@@ -0,0 +1,24 @@
+----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.basicBuildingBlocks_package.all;
+
+entity enhancedPwm is
+    PORT ( clk : in  STD_LOGIC;
+
+	);		   
+end enhancedPwm;
+
+architecture structure of enhancedPwm is
+
+    signal pwmCount_int: STD_LOGIC_VECTOR(7 downto 0);
+
+    
+    
+begin
+
+
+    
+    
+end structure;
